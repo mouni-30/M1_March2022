b31c95185273c2dadc6f145cf0c3e92ef78d5de5
mounika <101327261+mouni-30@users.noreply.github.com>
2022-03-30 22:05:25 +0530

    Update main.c

--- 2_implementation/main.c
+++ 2_implementation/main.c
@@ -1,41 +1,76 @@
-PROJ_NAME = main
-TEST_PROJ_NAME = Test_$(PROJ_NAME)
+#include <stdio.h>
+ int addition();
+int subtraction();
+int multiplication();
+int division();
+int modulus();
+int power();
+int factorial(); // it is header file, conatins all function
+int main() {
+  // variable declaration
+  int choice; 
+  int n1,n2, ans=0;
+  printf("Select the operation you want perform");
+  printf("1.Additon\n 2.Subtraction\n 3.multplication \n 4.Division \n 5.Modules\n 6.Power\n 7.Factorial \n ");
+  scanf("%d", &choice);
 
-SRC = src/addition.c\
-src/subtraction.c\
-src/division.c\
-src/modules.c\
-src/multiplication.c\
-src/factorial.c\
-src/power.c\
+// switch statement
+ switch (choice) {
 
+    case 1:
+      printf("Enter two number");
+      scanf("%d %d",&n1,&n2);
+      
+      ans = addition(n1,n2); //calling addition function
+      
+      printf("answer=%d",ans);
+      break;
+    case 2:
+     printf("Enter two number");
+      scanf("%d %d",&n1,&n2);
+      ans=subtraction(n1,n2); //calling subtraction function
+      printf("answer=%d",ans);
+      break;
+    case 3:
+      printf("Enter two number");
+      scanf("%d %d",&n1,&n2);
+      ans = multiplication(n1,n2); //calling multiply function 
+      printf("answer=%d",ans);
+      break;
+    case 4:
+      printf("Enter two number");
+      scanf("%d %d",&n1,&n2);
+     
+     ans = division(n1,n2);    //calling division function
+     printf("answer=%d",ans);
 
+      break;
+    case 5: 
+        printf("Enter two number");
+        scanf("%d %d",&n1,&n2);
+       ans= modulus(n1,n2);  // calling madules function
+       printf("answer=%d",ans);
+        break;  
+    case 6: 
+        printf("\nEnter two numbers to find the power \n");
+        printf("number: ");
+        scanf("%d",&n1);
+ 
+        printf("power : ");
+        scanf("%d",&n2);      // calling power function
+       ans= power(n1,n2);
+          printf("answer=%d",ans);
+        break;  
+    case 7: 
+        printf("\nEnter a number to find factorial : ");
+        scanf("%d",&n1);
+        ans = factorial(n1); //calling  factorial function
+        printf("answer=%d",ans);
+        break;  
+    // operator doesn't match any case constant
+    default:
+      printf("Error! operator is not correct");
+  }
 
-INC_H = inc
-INC_T = unity
-
-ifdef OS
-   RM = del 
-   FixPath = $(subst /,\,$1)
-   EXEC = exe
-else
-   ifeq ($(shell uname), Linux)
-      RM = rm -rf
-      FixPath = $1
-	  EXEC = out
-   endif
-endif
-
-
-all:
-	gcc -I $(INC_H) $(SRC) main.c -o $(call FixPath,$(PROJ_NAME).$(EXEC))
-	
-run:
-	$(call FixPath,$(PROJ_NAME).$(EXEC))
-test:
-	gcc -I $(INC_H) -I $(INC_T) $(TEST_SRC) $(SRC) -o $(call FixPath,$(TEST_PROJ_NAME).$(EXEC))
-	./$(call FixPath,$(TEST_PROJ_NAME).$(EXEC))
-
-clean:
-	
-	$(RM) *.$(EXEC)
+  return 0;
+}
